2bf99239804929c97c526445ea482d79b2c3efde
Marcin Tichoniuk <marcin@sology.eu>
2026-01-11 08:52:39 +0100

    feat: add json middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 const PORT = 3000;
+
+// middlewares
+app.use(json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
