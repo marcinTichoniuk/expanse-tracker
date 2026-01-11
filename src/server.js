2c29f960f262c90da88f9ece57793ccfbdc1e1c5
Marcin Tichoniuk <marcin@sology.eu>
2026-01-11 10:29:05 +0100

    feat: add startServer function

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express, { json } from 'express';
+import { connectDB } from './db/dbConfig.js';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,18 @@ app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Hello World!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-});
+// start server
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.error(`Failed to start the server: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
